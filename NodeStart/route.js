337665048c839842169df86c3eadb7e72ae04582
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-01-10 20:35:03 +0500

    Create route.js

---
+++ NodeStart/route.js
@@ -0,0 +1,31 @@
+// Routing 
+
+const url = require('url');
+const http = require('http');
+
+const server = http.createServer((req, res)=> {
+    
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+
+        console.log(req.url);
+        res.end("This is the overview");
+    }
+    else if (pathName === '/product'){
+        
+        console.log(req.url);
+        res.end("This is the product");
+    }else{
+        res.writeHead(404,{
+            'Conteent-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page is not found<h1>');
+    }
+
+});
+
+server.listen(8000,'127.0.0.1', ()=> {
+    console.log('listening request on port 8000');
+});
